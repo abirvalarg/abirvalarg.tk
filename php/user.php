0134c4a1f03ae14626a759a597f4e14620c2cec8
abirvalarg <yurnik2004@gmail.com>
2021-06-19 02:33:10 +0300

    user functions

--- php/user.php
+++ php/user.php
@@ -2,19 +2,78 @@
 require_once 'db.php';
 
 class User {
-	function __construct(int $uid) {
+	public ?int $uid = NULL;
+	public ?string $login = NULL;
+	public ?bool $isActive = NULL;
+	public ?bool $isAdmin = NULL;
+	public ?string $dispName = NULL;
+
+	function __construct($uid) {
+		$conn = db();
+		switch(gettype($uid)) {
+		case 'integer':
+			$this->uid = $uid;
+			$stmt = $conn->prepare('SELECT login, isActive, isAdmin, dispName FROM user WHERE id=?');
+			$stmt->bind_param('i', $uid);
+			break;
+
+		case 'string':
+			$this->login = $uid;
+			$stmt = $conn->prepare('SELECT id, isActive, isAdmin, dispName FROM user WHERE login=?');
+			$stmt->bind_param('s', $uid);
+			break;
+		}
+		if($stmt->execute()) {
+			$stmt->store_result();
+			if($stmt->num_rows == 1) {
+				$stmt->bind_result($idLogin, $this->isActive, $this->isAdmin, $this->dispName);
+				$stmt->fetch();
+				if($this->uid === NULL) $this->uid = $idLogin;
+				else $this->login = $idLogin;
+			} else $this->uid = $this->login = NULL;
+		} else $this->uid = $this->login = NULL;
+		$stmt->close();
 	}
 
-	public static function login(string $login, string $password) : User {
-		return new User(0);
+	public function save() : Bool {
+		return false;
+	}
+
+	public static function login(string $login, string $password) : ?User {
+		$result = NULL;
+		$conn = db();
+		$stmt = $conn->prepare('SELECT id, password FROM user WHERE login=?');
+		$stmt->bind_param('s', $login);
+		$stmt->execute();
+		$stmt->store_result();
+		if($stmt->num_rows == 1) {
+			$stmt->bind_result($uid, $passHash);
+			$stmt->fetch();
+			if(password_verify($password, $passHash)) $result = new User($uid);
+		}
+		$stmt->close();
+		return $result;
 	}
 
 	public static function register(string $login, string $password) : Bool {
-		return false;
+		$conn = db();
+		$stmt = $conn->prepare('INSERT INTO user (login, password) VALUES (?, ?)');
+		$passHash = password_hash($password, PASSWORD_DEFAULT);
+		$stmt->bind_param('ss', $login, $passHash);
+		return $stmt->execute();
 	}
 
 	public static function exists(string $login) : Bool {
-		return false;
+		$conn = db();
+		$stmt = $conn->prepare('SELECT id FROM user WHERE login=?');
+		$stmt->bind_param('s', $login);
+		if($stmt->execute()) {
+			$stmt->store_result();
+			$result = $stmt->num_rows == 1;
+		} else
+			$result = false;
+		$stmt->close();
+		return $result;
 	}
 };
 ?>
